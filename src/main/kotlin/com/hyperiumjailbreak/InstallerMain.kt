104dafc5742e35ca2870904c55aeb9b947b895b6
Reece Dunham <me@rdil.rocks>
2019-09-08 17:08:07 -0400

    bare bones tornadofx start

--- src/main/kotlin/com/hyperiumjailbreak/InstallerMain.kt
+++ src/main/kotlin/com/hyperiumjailbreak/InstallerMain.kt
@@ -1,6 +1,11 @@
 package com.hyperiumjailbreak
 
+import com.hyperiumjailbreak.fx.InstallerApp
+import tornadofx.*
+
+@Suppress("unused")
 class InstallerMain {
-    fun main() {
+    fun main(args: Array<String>) {
+        launch<InstallerApp>(args)
     }
 }
---
+++ src/main/kotlin/com/hyperiumjailbreak/fx/InstallerApp.kt
@@ -0,0 +1,5 @@
+package com.hyperiumjailbreak.fx
+
+import javafx.application.Application
+
+abstract class InstallerApp : Application()
