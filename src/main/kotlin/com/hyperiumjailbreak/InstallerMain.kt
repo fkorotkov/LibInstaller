aa2450d0eb41dc834a210ba6a8ae8da4fb9a48f3
RoundishCafe156 <oldchachyboy@hotmail.com>
2019-09-11 17:52:54 +0100

    Kotlin cleanup :)

--- src/main/kotlin/com/hyperiumjailbreak/InstallerMain.kt
+++ src/main/kotlin/com/hyperiumjailbreak/InstallerMain.kt
@@ -2,18 +2,11 @@ package com.hyperiumjailbreak
 
 import com.hyperiumjailbreak.stylesheet.MaterialStylesheet
 import com.hyperiumjailbreak.view.StartupView
-import javafx.scene.image.Image
-import javafx.stage.Stage
 import tornadofx.App
-import tornadofx.FX
 import tornadofx.launch
 
-
-object InstallerMain {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        launch<InstallerApp>()
-    }
+fun main() {
+    launch<InstallerApp>()
 }
 
 class InstallerApp : App(StartupView::class, MaterialStylesheet::class)
