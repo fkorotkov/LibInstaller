c06c0dd249f1f337de9d381a76eca6b4375c2792
Reece Dunham <me@rdil.rocks>
2019-09-12 12:19:14 -0400

    progress in kotlin !

--- src/main/kotlin/com/hyperiumjailbreak/Install.kt
+++ src/main/kotlin/com/hyperiumjailbreak/Install.kt
@@ -1,12 +1,42 @@
 package com.hyperiumjailbreak
 
 import cc.hyperium.installer.ProtectionDomain
+import org.apache.commons.io.FileUtils
+import com.google.common.io.Files
+import java.io.File
+import java.io.IOException
 
 class Install {
     fun do() {
         @JvmStatic val sep = File.separator;
-        val mc = new File(config.getDir());
+        val mc = File(config.getDir());
 
         mc.exists() ? InstallerMain.logger.info("Found MC Dir") : InstallerMain.fail("No MC Dir")
+
+        val versions = File(mc, "versions")
+        val origin = File(versions, "1.8.9")
+        val originJson = File(origin, "1.8.9.json")
+        val originJar = File(origin, "1.8.9.jar")
+        (!origin.exists() || !originJson.exists() || !originJar.exists()) ? InstallerMain.fail("You need to run Minecraft 1.8.9 first!") : () -> {}
+
+        val target = File(versions, "Hyperium 1.8.9")
+        val libs = File(mc, "libraries")
+
+        if(target.exists()) {
+            try FileUtils.deleteDirectory(target)
+            catch (e: Exception) InstallerMain.fail("Couldn't delete old game files, if MC is running, close it and try again")
+        }
+
+        val local = File(ProtectionDomain.class.getProtectionDomain().getCodeSource().getLocation().toURI())
+        val localLib = File(libraries, "cc" + sep + "hyperium" + sep + "Hyperium" + sep + "LOCAL" + sep + "Hyperium-LOCAL.jar")
+        try {
+            localLib.getParentFile().mkdirs()
+            Files.copy(local, new File(localLib.getParent(), localLib.getName())
+        } catch (e: IOException) {
+            InstallerMain.logger.fatal(e.getMessage())
+            InstallerMain.fail("Couldn't copy local Jar")
+        }
+
+        val tmpDir = File(java.nio.file.Files.createTempDirectory("Hyperium").toFile())
     }
 }
